1d4144a6a86db1400940ac9341eef17a5bec4e29
JanineZielman <janine.zielman@icloud.com>
2021-08-07 11:23:13 +0200

    removing sharp

--- src/templates/artist.js
+++ src/templates/artist.js
@@ -5,38 +5,30 @@ import { Helmet } from 'react-helmet';
 import Div100vh from 'react-div-100vh';
 import { ImageOrientation } from "../components/utils/image-orientation";
 
-import "react-responsive-carousel/lib/styles/carousel.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// import "react-responsive-carousel/lib/styles/carousel.css"; // requires a loader
+// import { Carousel } from "react-responsive-carousel";
 import "../components/styles/artist.css";
 
 import exitBlack from "../../public/icons/exit-black.png";
 
-
-
-
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  touchThreshold: 1000,
+  draggable: true,
+  lazyLoad: 'ondemand'
+};
 
 
 const Artist = ({ data }) => {
-  // index image
-  // src={data.prismicArtist.data.index_image.fluid.srcWebp}
-  // srcSet={data.prismicArtist.data.index_image.fluid.srcSetWebp}  
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [totalSlides, setTotalSlides] = useState(null);
-
-  useEffect(() => {
-    setCurrentSlide(0);
-  }, []);
-
-  useEffect(() => {
-    setTotalSlides(data.prismicArtist.data.gallery.length);
-  }, []);
-
-  const updateCurrentSlide = index => {
-    if (currentSlide !== index) {
-      setCurrentSlide(index);
-    }
-  };
-
   // back button that returns you to the right place on index
   const BackButton = () => {
     return (
@@ -46,22 +38,20 @@ const Artist = ({ data }) => {
     )
   }  
 
-  // console.log(data.prismicArtist.data.gallery);
-    const artistGallery = data.prismicArtist.data.gallery.filter(artist => artist.artist_image.fluid !== null)
-      .map((artist, index) => (
-      <img
-        key={`artist_image_${index}`}
-        className={`artist-artist-img ${ImageOrientation(
-                artist.artist_image
-              )}`}
-        src={artist.artist_image.fluid.srcWebp}
-        srcSet={artist.artist_image.fluid.srcSetWebp}
-        // alt={content.index_image.alt}
-        // loading="lazy"
-      />
-    ));    
+  const artistGallery = data.prismicArtist.data.gallery.filter(artist => artist.artist_image.fluid !== null)
+    .map((artist, index) => (
+    <img
+      key={`artist_image_${index}`}
+      className={`artist-artist-img ${ImageOrientation(
+              artist.artist_image
+            )}`}
+      data-lazy={artist.artist_image.fluid.srcWebp}
+      srcSet={artist.artist_image.fluid.srcSetWebp}
+    />
+  ));    
 
   return (  
+    <>
     <div className='single-artist-page-con'>
 
       <Helmet>
@@ -72,31 +62,16 @@ const Artist = ({ data }) => {
 
       <BackButton />
 
-    <Div100vh>
-     <Carousel
-        showIndicators={false}
-        showThumbs={false}
-        infiniteLoop={true}
-        transitionTime={0}
-        showStatus={false}
-        onChange={index => updateCurrentSlide(index)}
-      >
-        {artistGallery}
-      </Carousel>  
-    </Div100vh>
+      <div className="img-slider">
+        <Slider {...settings}>
+          {artistGallery}
+        </Slider>
+      </div>
 
 
-      <div className="artist-artist-title-and-counter-con">
-        <p>
-          {data.prismicArtist.data.artist_title.text} {" "}
-          <span className='counter'>
-            ({currentSlide + 1}/{totalSlides})
-          </span>
-        </p>
-      </div>     
     </div>
   
-
+    </>
   );
 };
 
